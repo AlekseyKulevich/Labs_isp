0de5abac9234685492bd48c016af28ecaa352f00
Lesha <kulevich.01@gmail.com>
2021-06-06 23:15:00 +0300

    add docker-compose

--- Lab3-4/TaskManager/TaskManager/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from django.contrib import admin
-
-urlpatterns = [
-    path('grappelli/', include('grappelli.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('taskManager.urls')),
-]
